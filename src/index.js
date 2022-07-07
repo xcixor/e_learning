ad4246e173c169bfbecd05884da8d6c2e5f6a614
xcixor <pndungu54@gmail.com>
2022-07-07 11:31:23 +0300

    chore(content): add initial app structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function Test(){
-    return <h2>Hello World!</h2>
-}
-
-render(<Test/>, document.getElementById('app'));
+render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById("app")
+);

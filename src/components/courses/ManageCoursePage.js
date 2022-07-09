c76e1a37cf8bd34fb4bf6293f769c9e81aaba832
xcixor <pndungu54@gmail.com>
2022-07-09 13:46:01 +0300

    feat(courses): add a form component to save and edit a course

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,15 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+function ManageCoursePage({
+	courses,
+	authors,
+	loadAuthors,
+	loadCourses,
+	saveCourse,
+	history,
+	...props
+}) {
+	const [course, setCourse] = useState({ ...props.course });
+	const [errors, setErrors] = useState({});
 	useEffect(() => {
 		if (courses.length === 0) {
 			loadCourses().catch((error) => {
 				alert("Loading courses failed" + error);
 			});
+		}else {
+			setCourse({...props.course});
 		}
 
 		if (authors.length === 0) {
@@ -17,12 +31,31 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
 				alert("Loading authors failed" + error);
 			});
 		}
-	}, []);
+	}, [props.course]);
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setCourse((prevCourse) => ({
+			...prevCourse,
+			[name]: name === "authorId" ? parseInt(value, 10) : value,
+		}));
+	}
+
+	function handleSave(event) {
+		event.preventDefault();
+		saveCourse(course).then(() => {
+			history.push("/courses");
+		});
+	}
 
 	return (
-		<div>
-			<h2>Manage Course</h2>
-		</div>
+		<CourseForm
+			course={course}
+			errors={errors}
+			authors={authors}
+			onChange={handleChange}
+			onSave={handleSave}
+		/>
 	);
 }
 
@@ -31,18 +64,32 @@ ManageCoursePage.propTypes = {
 	courses: PropTypes.array.isRequired,
 	loadCourses: PropTypes.func.isRequired,
 	loadAuthors: PropTypes.func.isRequired,
+	course: PropTypes.object.isRequired,
+	saveCourse: PropTypes.func.isRequired,
+	history: PropTypes.object.isRequired,
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+	return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+	const slug = ownProps.match.params.slug;
+	const course =
+		slug && state.courses.length > 0
+			? getCourseBySlug(state.courses, slug)
+			: newCourse;
 	return {
 		courses: state.courses,
 		authors: state.authors,
+		course,
 	};
 }
 
 const mapDispatchToProps = {
 	loadCourses,
 	loadAuthors,
+	saveCourse,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);

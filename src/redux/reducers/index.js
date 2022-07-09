6a7a200abb3d2be66ac7ca16e50d12efaf3a5512
xcixor <pndungu54@gmail.com>
2022-07-09 15:02:20 +0300

    chore(api): add an api status action to track the status of api calls in thunks

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
 import authors from "./authorReducer";
+import apiCallStatusReducer from "./apiStatusReducer";
 
 const rootReducer = combineReducers({
 	courses,
 	authors,
+	apiCallStatusReducer,
 });
 
 export default rootReducer;

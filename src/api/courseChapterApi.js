3f566501bd3ec1fbd6128ad1b2eb5ba51b39c0e0
xcixor <pndungu54@gmail.com>
2022-07-11 19:06:35 +0300

    chore(api): setup api to create and delete course chapters
    [Fixes #12]

--- src/api/courseChapterApi.js
+++ src/api/courseChapterApi.js
@@ -6,3 +6,19 @@ export function getCourseChapters(courseId) {
 		.then(handleResponse)
 		.catch(handleError);
 }
+
+export function saveCourseChapter(chapter) {
+	return fetch(baseUrl + (chapter.id || ""), {
+		method: chapter.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify(chapter),
+	})
+		.then(handleResponse)
+		.catch(handleError);
+}
+
+export function deleteCourseChapter(courseId) {
+	return fetch(baseUrl + courseId, { method: "DELETE" })
+		.then(handleResponse)
+		.catch(handleError);
+}
